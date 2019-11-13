b7f1c09fffc5edeb3a85203e715c1d2bc4f3dc8b
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-11-13 11:38:49 +0800

    新增 rollup 插件配置项

---
+++ rollup/defaultOptions.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+
+  // 需要进行压缩的文件后缀
+  extensions: [ '.js' ],
+
+  // 需要包含的文件
+  include: undefined,
+
+  // 需要排除的文件
+  exclude: undefined
+
+};
--- rollup/index.js
+++ rollup/index.js
@@ -1,10 +1,29 @@
+require('@moomfe/zenjs');
 const minifier = require('../src/index.js');
+const defaultOptions = require('./defaultOptions.js');
+const { extname } = require('path');
+const { createFilter } = require('rollup-pluginutils');
 
 
-module.exports = function( option = {} ){
+module.exports = function( userOptions ){
+  const options = Object.$assign( {}, defaultOptions, userOptions );
+  const extensions = options.extensions || [];
+
+  if( extensions.length === 0 ){
+    return;
+  }
+
+  var filter = createFilter( options.include, options.exclude );
+
   return {
-    name: 'hu-template-minifier',
     transform( code, id ){
+      if( !extensions.includes( extname( id ) ) ){
+        return;
+      }
+      if( !filter( id ) ){
+        return;
+      }
+
       return minifier( code, id );
     }
   };
